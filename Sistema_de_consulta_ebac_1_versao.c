99c041bd5c4802774e53f45a73b4b715c02eba45
ClintSant <ergonkoine@gmail.com>
2024-09-15 09:35:05 -0300

    Opção de saída.
    Foi adicionado ao código uma 4ª funcionalidade: sair do sistema.

--- Sistema_de_consulta_ebac_1_versao.c
+++ Sistema_de_consulta_ebac_1_versao.c
@@ -135,6 +135,7 @@ int main()
 	printf("\t1 - Registrar nomes\n\n");
 	printf("\t2 - Consultar nomes\n\n");
 	printf("\t3 - Deletar nomes\n\n");
+	printf("\t4 - Sair do sistema\n\n");
 	
 	printf("Opção:"); //fim do menu//
 	
@@ -159,6 +160,10 @@ int main()
 		
 		break;
 		
+		case 4:
+		printf("Obrigado por utilizar o sistema!\n");
+		return 0;
+		break;
 		
 		
 		
